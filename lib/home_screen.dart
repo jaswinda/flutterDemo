268598928408f9d2381d97f0eed0fd389bede1ad
jaswinda <singhjassi2624@gmail.com>
2022-07-26 16:00:15 +0545

    [feat] row and Column

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,10 +6,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Container(),
+        body: SingleChildScrollView(
+      child: Column(children: [
+        for (int i = 0; i < 100; i++)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
+        myColorfulRow(50.0),
+      ]),
+    ));
+  }
+
+  Widget myColorfulRow(double height) {
+    return Row(
+      children: [
+        Container(
+          width: 100,
+          height: height,
+          color: Colors.blue,
+        ),
+        Expanded(
+          child: Container(
+            width: 100,
+            height: height,
+            color: Colors.pink,
+          ),
+        ),
+        Expanded(
+          child: Container(width: 100, height: 100, color: Colors.red),
+        ),
+      ],
     );
   }
 }
