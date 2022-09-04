3aff6c3fb83df0cd9ee0bd0efaf875e5a1fedb8e
jaswinda <singhjassi2624@gmail.com>
2022-09-04 16:12:29 +0545

    image upload

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
@@ -15,19 +16,27 @@ class ProductController extends GetxController {
     loading.value = true;
     data['token'] = await authService.getToken();
     var url = Uri.parse(ADD_PRODUCT_API);
-    await Future.delayed(const Duration(seconds: 3));
-    var response = await http.post(url, body: data);
-    loading.value = false;
+    //multipart that takes file
+    var request = http.MultipartRequest('POST', url);
+    request.fields.addAll(data);
+    if (file != null) {
+      var pic = await http.MultipartFile.fromPath('image', file.path);
+      request.files.add(pic);
+    }
+    var response = await request.send();
+
     if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-      if (jsonResponse["success"]) {
+      var decodedResponse = jsonDecode(await response.stream.bytesToString());
+      if (decodedResponse['success']) {
         Get.back();
-        showMessage(message: jsonResponse["message"]);
+        showMessage(message: decodedResponse['message']);
       } else {
-        showMessage(message: jsonResponse["message"], isSuccess: false);
+        showMessage(message: decodedResponse['message'], isSuccess: false);
       }
+      loading.value = false;
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      loading.value = false;
+      Get.snackbar('Error', 'Something went wrong');
     }
   }
 }
--- lib/views/pages/forms/add_edit_product_form.dart
+++ lib/views/pages/forms/add_edit_product_form.dart
@@ -29,79 +29,93 @@ class _AddEditProductFormState extends State<AddEditProductForm> {
 
   final TextEditingController _descriptionController = TextEditingController();
   final ProductController productController = Get.put(ProductController());
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController categoryIdController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // height: 300,
-        color: backgroundColor,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(widget.title, style: const TextStyle(fontSize: 20)),
-                Column(
-                  children: [
-                    MyField(
-                      text: "Category Name",
-                      controller: _nameController,
-                    ),
-                    MyField(
-                      text: "Category description",
-                      controller: _descriptionController,
-                    ),
-                  ],
-                ),
-                InkWell(
-                    onTap: () async {
-                      pickedFile = await _picker.getImage(
-                        source: ImageSource.gallery,
-                      );
-                      setState(() {});
-                      if (pickedFile != null) {
-                        // Get.back();
-                      }
-                    },
-                    child: (pickedFile == null)
-                        ? Container(
-                            width: Get.width / 2,
-                            height: Get.height / 4,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: const Icon(Icons.add_a_photo),
-                          )
-                        : Container(
-                            width: Get.width / 2,
-                            height: Get.height / 4,
-                            decoration: BoxDecoration(
-                              //image from file
-                              image: DecorationImage(
-                                  image: FileImage(
-                                    File(pickedFile!.path),
-                                  ),
-                                  fit: BoxFit.cover),
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: const Icon(Icons.add_a_photo),
-                          )),
-                MyButton(
-                    onTap: () {
-                      if (_formKey.currentState!.validate()) {
-                        var data = {
-                          "name": _nameController.text,
-                          "description": _descriptionController.text
-                        };
-                        productController.add(data, pickedFile);
-                      }
-                    },
-                    text: 'Save'),
-              ],
+      body: SingleChildScrollView(
+        child: Container(
+          // height: 300,
+          color: backgroundColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.title, style: const TextStyle(fontSize: 20)),
+                  Column(
+                    children: [
+                      MyField(
+                        text: "Category Name",
+                        controller: _nameController,
+                      ),
+                      MyField(
+                        text: "Category description",
+                        controller: _descriptionController,
+                      ),
+                      MyField(
+                        text: "Price",
+                        controller: priceController,
+                      ),
+                      MyField(
+                        text: "Category ID",
+                        controller: categoryIdController,
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                      onTap: () async {
+                        pickedFile = await _picker.getImage(
+                          source: ImageSource.gallery,
+                        );
+                        setState(() {});
+                        if (pickedFile != null) {
+                          // Get.back();
+                        }
+                      },
+                      child: (pickedFile == null)
+                          ? Container(
+                              width: Get.width / 2,
+                              height: Get.height / 4,
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Icon(Icons.add_a_photo),
+                            )
+                          : Container(
+                              width: Get.width / 2,
+                              height: Get.height / 4,
+                              decoration: BoxDecoration(
+                                //image from file
+                                image: DecorationImage(
+                                    image: FileImage(
+                                      File(pickedFile!.path),
+                                    ),
+                                    fit: BoxFit.cover),
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Icon(Icons.add_a_photo),
+                            )),
+                  MyButton(
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          var data = {
+                            "name": _nameController.text,
+                            "description": _descriptionController.text,
+                            "category_id": categoryIdController.text,
+                            "price": priceController.text
+                          };
+                          productController.add(data, pickedFile);
+                        }
+                      },
+                      text: 'Save'),
+                ],
+              ),
             ),
           ),
         ),
